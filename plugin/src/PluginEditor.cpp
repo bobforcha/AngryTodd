f7defb57f51e9241e7cde33ff2e79b4e7a4e3b56
Bob Forcha <bobforcha@gmail.com>
2026-01-06 13:09:21 -0500

    Begin basic GUI layout

--- plugin/src/PluginEditor.cpp
+++ plugin/src/PluginEditor.cpp
@@ -8,7 +8,8 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
     juce::ignoreUnused (processorRef);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (800, 300);
+
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
@@ -19,11 +20,11 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
 void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll (juce::Colours::aquamarine);
+    
+    g.setColour (juce::Colours::beige);
+    g.setFont (45.0f);
+    g.drawFittedText ("Angry Todd", getLocalBounds(), juce::Justification::topLeft, 1);
 }
 
 void AudioPluginAudioProcessorEditor::resized()
--- plugin/src/PluginProcessor.cpp
+++ plugin/src/PluginProcessor.cpp
@@ -130,6 +130,9 @@ void AudioPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
+    // rawVolume param
+    // rawVolume = 0.15f;
+
     // In case we have more outputs than inputs, this code clears any output
     // channels that didn't contain input data, (because these aren't
     // guaranteed to be empty - they may contain garbage).
